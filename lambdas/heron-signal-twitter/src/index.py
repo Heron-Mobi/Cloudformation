d2e33d27f240cd2d907e0a89adf8bccdb7654153
Jason Smith <jasonrichardsmith@pm.me>
2024-02-28 10:44:48 +0100

    fixing twitter handler, works, update all python versions, export tweepy layer

--- lambdas/heron-signal-twitter/src/index.py
+++ lambdas/heron-signal-twitter/src/index.py
@@ -1,8 +1,8 @@
 import boto3
 import json
 from string import Template
-from tweepyoverrides.tweepyoverrides import TweepyRefreshAuth2UserHandler
 import tweepy
+from tweepyoverrides.tweepyoverrides import TweepyRefreshAuth2UserHandler
 
 
 def lambda_handler(event, context):
@@ -11,15 +11,15 @@ def lambda_handler(event, context):
     identityid = payload["identityID"]
     twitter_handle = payload["config"]["twitter-handle"]
 
-
-
-
     ssm = boto3.client("ssm")
-    s3 = boto3.client("s3")
+    s3 = boto3.resource("s3")
     secretsmanager = boto3.client("secretsmanager")
 
     video_domain_param = ssm.get_parameter(
         Name="/heron/heron-video-domain", WithDecryption=False
+    )
+    dashboard_domain_param = ssm.get_parameter(
+        Name="/heron/heron-dashboard-domain", WithDecryption=False
     )
     video_bucket_param = ssm.get_parameter(
         Name="/heron/video-bucket-name", WithDecryption=False
@@ -28,29 +28,23 @@ def lambda_handler(event, context):
         Name="/heron/config-bucket-name", WithDecryption=False
     )
     video_bucket = video_bucket_param["Parameter"]["Value"]
+    dash_domain = dashboard_domain_param["Parameter"]["Value"]
     config_bucket = config_bucket_param["Parameter"]["Value"]
     video_domain = video_domain_param["Parameter"]["Value"]
     twittersecretvalue = secretsmanager.get_secret_value(
         SecretId="heron/integrations/twitter"
     )
     twittersecret = json.loads(twittersecretvalue["SecretString"])
-    dash_domain_param = ssm.get_parameter(
-        Name="/heron/heron-dashboard-domain", WithDecryption=False
-    )
-    dash_domain = dash_domain_param["Parameter"]["Value"]
     tokenObject = s3.Object(
         config_bucket,
         identityid + "/twittertoken",
     )
-    tokens = json.loads(tokenObject.get()['Body'].read())
-    
-
-
+    tokens = json.loads(tokenObject.get()["Body"].read())
 
     camtemplate = ""
-    with open('template.html', 'r') as file:
+    with open("template.html", "r") as file:
         camtemplate = Template(file.read())
-   
+
     camstring = camtemplate.substitute(
         date=date,
         identityid=identityid,
@@ -59,11 +53,9 @@ def lambda_handler(event, context):
     )
     s3 = boto3.resource("s3")
     camobject = s3.Object(
-        bucket_name=video_bucket,
-        key=identityid + "/" + date + "/twitter.html"
+        bucket_name=video_bucket, key=identityid + "/" + date + "/twitter.html"
     )
     camobject.put(Body=camstring, ContentType="text/html")
-
 
     auth = TweepyRefreshAuth2UserHandler(
         redirect_uri="https://" + dash_domain + "/integrations/twittertoken",
@@ -71,9 +63,19 @@ def lambda_handler(event, context):
         client_secret=twittersecret["clientsecret"],
         scope=["tweet.write", "offline.access", "tweet.read", "users.read"],
     )
-    newtokens = auth.refresh_token(tokens['refresh_token'])
+
+    newtokens = auth.refresh_token(tokens["refresh_token"])
 
     tokenObject.put(Body=json.dumps(newtokens).encode())
-    client = tweepy.Client(bearer_token=tokens['access_token'])
-    resp = client.create_tweet(text="heron test heron test", user_auth=False)
+    client = tweepy.Client(bearer_token=newtokens["access_token"])
+    resp = client.create_tweet(
+        text="https://"
+        + video_domain
+        + "/"
+        + identityid
+        + "/"
+        + date
+        + "/twitter.html",
+        user_auth=False,
+    )
     print(resp)
