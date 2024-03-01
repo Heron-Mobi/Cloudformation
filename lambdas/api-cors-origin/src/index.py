cf14fad1b2c233549a4ece474342feba1a65ec9e
Jason Smith <jasonrichardsmith@pm.me>
2024-03-01 11:35:57 +0100

    updated CORS rule and added email verification for cognito

--- lambdas/api-cors-origin/src/index.py
+++ lambdas/api-cors-origin/src/index.py
@@ -1,13 +1,13 @@
-import json
-import boto3
+import os
 
 
 def handler(event, context):
+
     response = {
         "isBase64Encoded": False,
         "statusCode": 200,
         "headers": {
-            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Origin": "https://" + os.environ["ORIGINDOMAIN"],
             "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
             "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,AccessAuthorization,X-Api-Key,X-Amz-Security-Token",
             "Access-Control-Allow-Credentials": True,
